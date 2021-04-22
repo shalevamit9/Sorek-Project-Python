44cbe56a54e0d898ba4e8de97a0b630f95303b2f
shalevamit9 <shalevamit9@gmail.com>
2021-04-23 01:07:49 +0300

    romved North/South Facility classes, added taoz cost, secondary taoz cost and energy limit to Facility class

--- app/classes.py
+++ app/classes.py
@@ -2,48 +2,27 @@ from enum import Enum
 
 
 class Facility:
-    def __init__(self, production_amount, kwh_per_hour_price, se_per_hour, number_of_pumps,
-                 water_cubic_meter_price):
+    def __init__(self, production_amount=None, taoz_cost=None, secondary_taoz_cost=None, se_per_hour=None,
+                 number_of_pumps=None, water_cubic_meter_price=None, kwh_energy_limit=None):
         self.production_amount = production_amount
-        self.kwh_per_hour_price = kwh_per_hour_price
+        self.taoz_cost = taoz_cost
+        self.secondary_taoz_cost = secondary_taoz_cost
         self.se_per_hour = se_per_hour
         self.number_of_pumps = number_of_pumps
         self.water_cubic_meter_price = water_cubic_meter_price
+        self.kwh_energy_limit = kwh_energy_limit
         self.production_price = None
 
     def calculate_price(self):
-        self.production_price = self.se_per_hour * self.production_amount * self.kwh_per_hour_price
-
-
-class NorthFacility(Facility):
-    def __init__(self, production_amount=None, se_per_hour=None, number_of_pumps=None,
-                 water_cubic_meter_price=None, kwh_per_hour_price=None):
-        super().__init__(production_amount, kwh_per_hour_price, se_per_hour, number_of_pumps, water_cubic_meter_price)
-
-
-class SouthFacility(Facility):
-    def __init__(self, production_amount=None, se_per_hour=None, number_of_pumps=None, water_cubic_meter_price=None,
-                 kwh_per_hour_price=None):
-        super().__init__(production_amount, kwh_per_hour_price, se_per_hour, number_of_pumps, water_cubic_meter_price)
+        self.production_price = self.se_per_hour * self.production_amount * self.taoz_cost
 
 
 class MatrixBullet:
-    def __init__(
-            self, south_production_amount=None, north_production_amount=None, taoz=None, number_of_pumps=None,
-            se_per_hour=None, kwh_per_hour=None, water_cubic_meters=None, date=None):
-        self.north_facility = NorthFacility()
-        self.south_facility = SouthFacility()
+    def __init__(self, taoz=None, date=None):
+        self.north_facility = Facility()
+        self.south_facility = Facility()
         self.taoz = taoz
         self.date = date
-        # self.price = self.se_per_hour * self.production_amount * self.kwh_per_hour
-        # self.north_price = self.se_per_hour * self.north_production_amount * self.kwh_per_hour
-        # self.south_price = self.se_per_hour * self.south_production_amount * self.kwh_per_hour
-        # self.north_price = None
-        # self.south_price = None
-
-    # def calculate_price(self):
-    #     self.north_price = self.se_per_hour * self.north_production_amount * self.kwh_per_hour
-    #     self.south_price = self.se_per_hour * self.south_production_amount * self.kwh_per_hour
 
     def define_taoz(self, taoz):
         taoz_to_assign = None
@@ -55,9 +34,6 @@ class MatrixBullet:
             taoz_to_assign = Taoz.PISGA
 
         self.taoz = taoz_to_assign
-
-    # def get_price(self):
-    #     return self.se_per_hour * self.production_amount * self.kwh_per_hour
 
     def get_week_day(self):
         return self.date.weekday()
